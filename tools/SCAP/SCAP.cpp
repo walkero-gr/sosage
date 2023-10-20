8924ddd5e2ebb124d57169236b18b1ebe91d92a0
Simon Giraudot <sosage@ptilouk.net>
2023-10-20 19:15:16 +0200

    Move SCAP in its own cmake project

--- tools/SCAP/SCAP.cpp
+++ tools/SCAP/SCAP.cpp
@@ -1,10 +1,4 @@
-#ifdef SOSAGE_SCAP // avoid main() conflicts with Sosage
-
 #include <Sosage/Utils/asset_packager.h>
-
-std::string Sosage::Asset_manager::folder_name = "";
-std::vector<Sosage::Buffer> Sosage::Asset_manager::buffers;
-Sosage::Package_asset_map Sosage::Asset_manager::package_asset_map;
 
 int main (int argc, char** argv)
 {
@@ -24,5 +18,3 @@ int main (int argc, char** argv)
 
   return EXIT_SUCCESS;
 }
-
-#endif
